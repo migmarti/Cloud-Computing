e220306b020132ba701418b3233332b0bf3439e0
Migmarti <mmart96@hotmail.com>
2017-08-26 18:38:09 -0700

    Latest sync

--- AssignmentCode/HelloWorldHttp.js
+++ AssignmentCode/HelloWorldHttp.js
@@ -1,14 +1,12 @@
 // Carga el modulo http para crear un servidor http.
 var http = require('http');
 
-// Configurar el servidor http para responder con "Hello World" a todos los requests
-var server = http.createServer(function (request, response) {
+// Configurar el servidor http para responder con "Hello World" a todos los requests.
+// Escucha en el puerto 8080.
+http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
-});
-
-// Escuchar en el puerto 8000 con el IP 127.0.0.1 (Local)
-server.listen(8000);
+}).listen(8080);
 
 // Mostrar un mensaje para informar que el servidor ya fue levantado.
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at http://127.0.0.1:8080/");
